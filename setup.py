f0da75ba4f164e1d0291efc5585fd371b14217c3
Roman Stolyarov <sadimer@ispras.ru>
2022-11-30 14:28:53 +0300

    Fix requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 import setuptools
 
-reqs = ['six', 'grpcio==1.37.1', 'grpcio-tools==1.37.1', 'pyyaml', 'graphlib_backport', 'tosca-parser@git+https://github.com/sadimer/tosca-parser']
+reqs = ['six', 'grpcio==1.37.1', 'grpcio-tools==1.37.1', 'pyyaml', 'graphlib_backport', 'clouni-tosca-parser']
 setuptools.setup(install_requires=reqs)
