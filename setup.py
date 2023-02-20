0691975ba87cce63ad443ea25880e265fb41c239
sadimer <roman.stolyarov@mail.ru>
2023-02-20 14:18:48 +0300

    Return to tosca-parser upstream

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 import setuptools
 
-reqs = ['six', 'grpcio==1.37.1', 'grpcio-tools==1.37.1', 'pyyaml', 'graphlib_backport', 'clouni-tosca-parser']
+reqs = ['six', 'grpcio==1.37.1', 'grpcio-tools==1.37.1', 'pyyaml', 'graphlib_backport', 'tosca-parser']
 setuptools.setup(install_requires=reqs)
